bcfa566f64b5d7a6e8f25e97259dce48caad4342
Epic Zero (planner) <planner@zenod.local>
2026-07-11 05:08:40 +0200

    feat(phylax): duplicate customer front

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,5 @@
 export { createApp } from "./app.js";
 export { createCallisthenesCustomerLayer } from "./callisthenesCustomerLayer.js";
+export { createPhylaxCustomerLayer } from "./phylaxCustomerLayer.js";
 export { createRingUnit } from "./ringUnit.js";
 export { createRingCustomerLayer } from "./ringCustomerLayer.js";
